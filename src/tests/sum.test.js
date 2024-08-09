6a299671a2fc1fa41fe1a9116c2962a9bb8cf95b
Gabriel Teixeira da Rocha <gabrieloteixeira113@gmail.com>
2024-08-09 19:16:40 +0100

    Delete src/tests directory

--- src/tests/sum.test.js
+++
@@ -1,9 +0,0 @@
-import { render } from '@testing-library/react';
-const { describe } = require('node:test');
-const sum = require('../Components/sum');
-
-describe("teste sum", ()=>{
-    test("Should render correctly"), () =>{
-        render(<Header/>)
-    }
-});
